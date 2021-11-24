87978488e454c6bce05441b773098d1b5ab1276b
SaniyaZuberi <63394041+SaniyaZuberi@users.noreply.github.com>
2021-11-24 17:07:52 +0530

    Delete src/com/tw/pro/classes/alYasmeen directory

--- src/com/tw/pro/classes/alYasmeen/AlYasmeenNonTeachers.java
+++
@@ -1,8 +0,0 @@
-package com.tw.pro.classes.alYasmeen;
-
-public class AlYasmeenNonTeachers {
-    public void displayStudents(){
-        AlYasmeenAdmin student = new AlYasmeenAdmin();
-        student.displayStudents();
-    }
-}
